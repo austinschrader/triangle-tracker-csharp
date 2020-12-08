01d1c600f65f7eeb8e8008a94f7a63aa88b8e46d
Austin Schrader <austin.jschrader@gmail.com>
2020-12-08 15:08:02 -0800

    Add passing equilateral logic

--- Triangle/Models/TriangleTracker.cs
+++ Triangle/Models/TriangleTracker.cs
@@ -4,7 +4,11 @@ namespace Triangle
   {
     public string EvaluateTriangle(int side1, int side2, int side3)
     {
-      if (side1 == side2 || side2 == side3 || side1 == side3)
+      if (side1 == side2 && side2 == side3)
+      {
+        return "equilateral";
+      }
+      else if (side1 == side2 || side2 == side3 || side1 == side3)
       {
         return "isosceles";
       }
