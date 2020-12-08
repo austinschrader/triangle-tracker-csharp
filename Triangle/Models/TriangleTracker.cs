f05f65c1646eb8f9729a0146ae1751533aee0db7
Austin Schrader <austin.jschrader@gmail.com>
2020-12-08 15:31:15 -0800

    Add passing test for IsTriangle is when it's true
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>

--- Triangle/Models/TriangleTracker.cs
+++ Triangle/Models/TriangleTracker.cs
@@ -20,9 +20,12 @@ namespace Triangle
 
     public bool IsTriangle(int side1, int side2, int side3)
     {
+      if (side1 <= (side2 + side3) && side2 <= (side3 + side1) && side3 <= (side1 + side2))
+      {
+        return true;
+      }
       return true;
     }
-
+    
   }
-}
 }
